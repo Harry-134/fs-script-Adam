a7707b99a18fbfa289bf2e1359940b2ce80647b8
Adam <adamjona@live.se>
2026-01-15 15:44:29 -0600

    Added OS safety checks, and a print statement for it

--- projekt/login_monitor.py
+++ projekt/login_monitor.py
@@ -2,10 +2,18 @@
 import os
 import sys
 import argparse
+import platform
 from datetime import datetime
 
 # Current version
 VERSION = "1.0.0"
+
+#Checks if the current OS is linux based
+def check_os():
+    current_os = platform.system()
+    if current_os != "Linux":
+        print(f"Error: This script is only for Linux based systems. Currently on {current_os}")
+        sys.exit(1)
 
 #Flags and arguments
 def parse_arguments():
@@ -15,12 +23,17 @@ def parse_arguments():
 
 #If no command is given, gives user, time and startup message 
 def main():
+
+    check_os()
     args = parse_arguments()
+
     user = os.getenv('USER') or "Unknown"
+
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"\n--- Login Monitor Started ---")
     print(f"User: {user}")
     print(f"Time: {current_time}")
+    print(f"OS: {platform.system()} {platform.release()}")
     print("--------------------------------")
 
 if __name__ == "__main__":
